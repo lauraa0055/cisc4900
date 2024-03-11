2f81ace56ac730fb9997a637384c864c2f41b481
lauraa0055 <laura0055@AriasXPS>
2024-03-11 15:19:37 -0400

    R:User can make one timer, only using minutes with its title. 1+ timer->bug:timer goes back and forth from previous to current timer

--- js/otherPage.js
+++ js/otherPage.js
@@ -55,3 +55,60 @@ function calculations(input, conversion_type){
 		return output;
 	throw "Invalid Input";
 }
+
+/*
+	- create list item
+	- create row div: class = row g-2
+	- create inner col 1: header 3: Timer Title
+	- create inner col 2: class = " col-auto" -> button to exit
+	- p: timer
+*/
+
+
+
+//Timer functions
+const MINUTE_IN_SECONDS = 60;
+const ONE_MIN = MINUTE_IN_SECONDS;
+const ONE_HOUR = ONE_MIN * 60;
+
+var SECONDS_WANTED;
+var HOURS_WANTED;
+function addTimer(){
+	var minute_input = document.getElementById('minutes').value;
+	var minute_output = ONE_MIN * minute_input;
+	var display = document.getElementById('test');
+	startTimer(minute_output, display);
+	
+	//name
+	var name_of_timer_input = document.getElementById('timer_name').value;
+	var change_name_of_timer = document.getElementById('timer_title');
+	change_name_of_timer.innerHTML = name_of_timer_input;
+}
+
+const SECOND_IN_MILISECOND = 1000;
+function startTimer(duration, display) {
+	var timer = duration, minutes, seconds;
+	setInterval(function () {
+		minutes = parseInt(timer / 60, 10);
+		seconds = parseInt(timer % 60, 10);
+
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		display.innerText = minutes + ":" + seconds;
+
+		if (--timer < 0) {
+			timer = duration;
+		}
+	}, SECOND_IN_MILISECOND);
+}
+
+let close = document.getElementsByClassName('btn-close');
+for(var i = 0; i < close.length; i++){
+		close[i].onclick = function(){
+			var elementToDelete = ((this.parentElement).parentElement).parentElement;	
+			elementToDelete.remove();
+		}
+}
+
+
